00c68c309cc0342c4e4fb969dea1b34e28925cba
Jeff Donaldson <fejsez@gmail.com>
2012-02-24 16:25:02 -0600

    first commit

---
+++ index.php
@@ -0,0 +1,45 @@
+<html>
+ <head>
+ 
+  	<script src="mootools-1.2.3-core-nc.js" type="text/javascript" charset="utf-8"></script>
+   	<script src="mootools-1.2.3.1-more.js" type="text/javascript" charset="utf-8"></script>
+   	<script src="/ElementStack.js" type="text/javascript" charset="utf-8"></script>
+   	<script type="text/javascript" charset="utf-8">
+		window.addEvents({
+			domready: function(){
+				new ElementStacks($$('img'), $('wrapper'));
+			}
+		});
+   	</script>
+   	<title>dog is my co-pilot. seriously have you seen Garbanzo?</title>
+  <link href="mine.css" rel="stylesheet" type="text/css" />
+  </head>
+ <body>
+<div id="outer">
+<div id="container">
+<div id="dbad"> 
+<h1>fejsez.com<h1>
+<a id="blog1" href="javascript:showonlyone('newboxes1');" ><img width="200" height="200" src="/blog.jpg" /></a>
+<a id="blur1" href="javascript:showonlyone('newboxes2');" ><img width="200" height="200" src="/blur.jpg" /></a>
+<a id="photos1" href="javascript:showonlyone('newboxes4');" ><img width="200" height="200" src="/tracks.jpg" /></a>
+<a id="garb1" href="javascript:showonlyone('newboxes3');" ><img width="200" height="200" src="/garb.jpg" /></a>
+<div id="newboxes1"><a href="http://blog.fejsez.com"> BLOG</a></div>
+<div id="newboxes2"> <a href="http://bike.fejsez.com">BIKE</a></div>
+<div id="newboxes3"> <a href="http://photos.fejsez.com">BLUR</a></div>
+<div id="newboxes4"> <a href="http://about.me/fejsez">ABOUT</a></div>
+
+</div>
+</div>
+</div>
+<!-- Site Meter -->
+<script type="text/javascript" src="http://sm4.sitemeter.com/js/counter.js?site=sm4fejsez">
+</script>
+<noscript>
+<a href="http://sm4.sitemeter.com/stats.asp?site=sm4fejsez" style="font-size: 5pt;" target="_top">
+sitemeter.com</a>
+</noscript>
+<!-- Copyright (c)2009 Site Meter -->
+
+
+
+</html>
---
+++ showone.js
@@ -0,0 +1,14 @@
+function showonlyone(thechosenone) {
+      var newboxes = document.getElementsByTagName("div");
+            for(var x=0; x<newboxes.length; x++) {
+                  name = newboxes[x].getAttribute("name");
+                  if (name == 'newboxes') {
+                        if (newboxes[x].id == thechosenone) {
+                        newboxes[x].style.display = 'block';
+                  }
+                  else {
+                        newboxes[x].style.display = 'none';
+                  }
+            }
+      }
+}
